b1c58350dbbed81e54df6ee7d076240a44557ac1
c9czamat <c9czamat@vasvari.org>
2024-12-13 10:03:46 +0100

    Főoldal működik

--- routes/dynamicRoutes.js
+++ routes/dynamicRoutes.js
@@ -2,44 +2,48 @@ import express from "express";
 import { dbQuery } from "../database.js";
 
 const generateDynamicRoutes = async (app) => {
-    try {
-        const pages = await dbQuery("SELECT pageTitle FROM pages;");
-        const pageTitles = pages.map(page => page.pageTitle);
+  try {
+    const pages = await dbQuery("SELECT pageTitle FROM pages;");
+    const pageTitles = pages.map((page) => page.pageTitle);
 
-        pageTitles.forEach(title => {
-            const route = `/api/pages/${title.replace(/á/g, "a")
-                .replace(/é/g, "e")
-                .replace(/ő/g, "o")
-                .replace(/ú/g, "u")
-                .replace(/ö/g, "o")
-                .replace(/ü/g, "u")
-                .replace(/ű/g, "u")
-                .replace(/í/g, "i")
-                .replace(/ó/g, "o")
-                .replace(/Á/g, "A")
-                .replace(/É/g, "E")
-                .replace(/Ő/g, "O")
-                .replace(/Ú/g, "U")
-                .replace(/Ö/g, "O")
-                .replace(/Ü/g, "U")
-                .replace(/Ű/g, "U")
-                .replace(/Í/g, "I")
-                .replace(/Ó/g, "O")
-                .replace(/\s+/g, "-")}`;
-            app.get(route, async (req, res, next) => {
-                try {
-                    const page = await dbQuery("SELECT * FROM page WHERE pageTitle = ?;", [title]);
-                    res.status(200).json(page);
-                } catch (err) {
-                    next(err);
-                }
-            });
+    pageTitles.forEach((title) => {
+      const route = `/${title
+        .replace(/á/g, "a")
+        .replace(/é/g, "e")
+        .replace(/ő/g, "o")
+        .replace(/ú/g, "u")
+        .replace(/ö/g, "o")
+        .replace(/ü/g, "u")
+        .replace(/ű/g, "u")
+        .replace(/í/g, "i")
+        .replace(/ó/g, "o")
+        .replace(/Á/g, "A")
+        .replace(/É/g, "E")
+        .replace(/Ő/g, "O")
+        .replace(/Ú/g, "U")
+        .replace(/Ö/g, "O")
+        .replace(/Ü/g, "U")
+        .replace(/Ű/g, "U")
+        .replace(/Í/g, "I")
+        .replace(/Ó/g, "O")
+        .replace(/\s+/g, "-")}`;
+      app.get(route, async (req, res, next) => {
+        try {
+          const page = await dbQuery(
+            "SELECT * FROM page WHERE pageTitle = ?;",
+            [title]
+          );
+          res.status(200).json(page);
+        } catch (err) {
+          next(err);
+        }
+      });
 
-            console.log(`Route created: GET ${route}`);
-        });
-    } catch (err) {
-        console.error("Error generating dynamic routes:", err);
-    }
+      console.log(`Route created: GET ${route}`);
+    });
+  } catch (err) {
+    console.error("Error generating dynamic routes:", err);
+  }
 };
 
 export default generateDynamicRoutes;
--- routes/titles.js
+++ routes/titles.js
@@ -3,9 +3,9 @@ import { dbQuery } from "../database.js";
 
 const router = express.Router();
 
-router.get("/fooldal", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const pages = await dbQuery("SELECT pageTitle FROM page;");
+    const pages = await dbQuery("SELECT pageTitle FROM pages;");
     const uzletek = pages.map((page) => page.pageTitle);
     res.render("Fooldal", { pageTitle: "Főoldal", uzletek: uzletek });
   } catch (err) {
